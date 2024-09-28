ecfe91881644893b20b3c58893e15078404c76bc
hovanphancoder <hovanphancoder@gmail.com>
2024-09-29 01:18:36 +0700

    Revert "đẩy code lên"
    This reverts commit 658da173a57e7085f84f2de87202b15c82d563ff.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header>
       <h1>Trang Chủ</h1>
-      <p>Chào mừng đến c,jjvhnvới trang web của chúng tôi!</p>
+      <p>Chào mừng đến với trang web của chúng tôi!</p>
     </header>
   );
   
