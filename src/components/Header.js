8012888f224288155440f92cd0ee0216f515305d
hovanphancoder <hovanphancoder@gmail.com>
2024-12-22 23:09:09 +0700

    test 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 const Header = () => {
-  return (
-    <header>
-      <h1>Trang Chủ</h1>
-      <p>Chào mừng đến với trang web của chúng tôi!</p>
-    </header>
-  );
-  
+    return ( <
+        header >
+        <
+        h1 > Trang Chủ < /h1> <
+        p > Chào mừng đến với trang web của chúng tôi! < /p> < /
+        header >
+    );
+
 };
 
 export default Header;
